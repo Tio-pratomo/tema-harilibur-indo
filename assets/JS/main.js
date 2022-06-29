52d265752000e25a1d13105bfebde0e58a8a3a25
tio <sulistiopratomo@gmail.com>
2022-06-29 18:16:28 +0700

    add:menambah dependency untuk kebutuhan project

---
+++ assets/JS/main.js
@@ -0,0 +1,45 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { showDisplayDigimonCard, showFilteringDigimonCard } from './data/digimonAPI.js';
+import { hoverEffect } from './utility/hover-effect.js';
+
+const getPages = document.body.dataset.page;
+
+switch (getPages) {
+    case 'landing':
+        /** 
+        MENERAPKAN HOVER EFFECT
+        params(el1 required,el2 required, el3 required)
+        */
+        hoverEffect(
+            document.querySelector('.main-landing__split--left'),
+            document.querySelector('.main-landing__split--right'),
+            document.querySelector('.main-landing')
+        );
+        break;
+
+    case 'digimon':
+        /*
+         *INIT AOS
+         */
+        AOS.init({
+            delay: 800,
+        });
+        /*
+         *MENAMPILKAN BEBERAPA KOMPONEN
+         *CARD DIGIMON
+         */
+        showDisplayDigimonCard(document.querySelector('.digimon-char'));
+
+        /*
+         *MENAMPILKAN BEBERAPA KOMPONEN
+         *CARD DIGIMON BERDASARKAN
+         *USER SELECTED
+         */
+        showFilteringDigimonCard(
+            document.querySelector('.digimon-char'),
+            document.querySelector('#digimon-search'),
+            document.querySelector('#level-digimon')
+        );
+        break;
+}
